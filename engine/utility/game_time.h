629e81dc26cf57c5d38b641e1ce4ee17d2ea3576
Jelle van Campen <jelle.vancampen@ugent.be>
2016-10-04 16:42:50 +0200

    Updated the CMakeLists.txt files to organize source files into source groups. Added a preliminary version of the GameTime class.

---
+++ engine/utility/game_time.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef ENGINE_UTILITY_GAMETIME_H_
+#define ENGINE_UTILITY_GAMETIME_H_
+
+namespace Engine {
+
+// Provides access to timing information of the game loop. 
+class GameTime {
+  // TODO (Jelle): implement the gametime class to provide info on time step 
+  // size, absolute time, frame number, and frame interpolation. 
+};
+
+} // namespace
+
+#endif // ENGINE_UTILITY_GAMETIME_H_
