d5f9483383930464caed6d832c05626b3c98bacd
Jelle van Campen <jelle.vancampen@ugent.be>
2016-10-05 15:25:03 +0200

    Add the outlines of new classes for logging functionality.

---
+++ engine/debugging/log_channel.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef ENGINE_DEBUGGING_LOGCHANNEL_H_
+#define ENGINE_DEBUGGING_LOGCHANNEL_H_
+
+namespace engine {
+
+  // Represents a channel that accepts log messages and sends them to an 
+  // output channel. 
+  class LogChannel {
+    // TODO(Jelle): implement the LogChannel class
+  };
+
+} // namespace
+
+#endif // ENGINE_DEBUGGING_LOGCHANNEL_H_
---
+++ engine/debugging/logging.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef ENGINE_DEBUGGING_LOGGING_H_
+#define ENGINE_DEBUGGING_LOGGING_H_
+
+namespace engine {
+
+  // Manages the logging of messages. Messages can be sent to different 
+  // logging channels to categorize them. Logging channels subsequently pipe 
+  // incoming messages to an output channel including: the terminal, and log 
+  // files. 
+  class Logging {
+    // TODO(Jelle): implement the Logging class
+  };
+
+} // namespace
+
+#endif // ENGINE_DEBUGGING_LOGGING_H_
