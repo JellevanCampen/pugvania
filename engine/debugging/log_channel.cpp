ddc0c64241f8eb822f16b4d5990777397d9e0389
Jelle van Campen <jelle.vancampen@ugent.be>
2016-10-06 11:18:22 +0200

    Implemented basic class structure of log channels.

---
+++ engine/debugging/log_channel.cpp
@@ -0,0 +1,10 @@
+#include "log_channel.h"
+
+namespace engine {
+
+void LogChannel::Log(std::string message) {
+  // TODO(Jelle): implement this method to send the message to all registered 
+  // output channels for this log channel.
+}
+
+} // namespace
--- engine/debugging/log_channel.h
+++ engine/debugging/log_channel.h
@@ -2,13 +2,26 @@
 #ifndef ENGINE_DEBUGGING_LOGCHANNEL_H_
 #define ENGINE_DEBUGGING_LOGCHANNEL_H_
 
+#include <string>
+
 namespace engine {
 
-  // Represents a channel that accepts log messages and sends them to an 
-  // output channel. 
-  class LogChannel {
-    // TODO(Jelle): implement the LogChannel class
-  };
+// Represents a channel that accepts log messages and sends them to an 
+// output channel. 
+class LogChannel {
+ public:
+   LogChannel(std::string name, std::string tag, bool timestamp = false)
+      : name_(name), 
+        tag_(tag), 
+        timestamp_(timestamp) {}
+
+   void Log(std::string message);
+
+ private:
+  std::string name_;
+  std::string tag_;
+  bool timestamp_{ false };
+};
 
 } // namespace
 
