7fc902727750c69d5a4246e79f443f089fb6108e
Jelle van Campen <jelle.vancampen@ugent.be>
2016-10-10 15:13:00 +0200

    Implement an initial version of the Path subsystem which is used to access resource directories for the project.

---
+++ engine/utility/path.cpp
@@ -0,0 +1,48 @@
+#include "utility\path.h"
+#include <sstream>
+#include <iostream>
+#include "boost/property_tree/ptree.hpp"
+#include "boost/property_tree/ini_parser.hpp"
+#include <boost/algorithm/string.hpp>
+
+namespace engine {
+
+Path::Path() {
+  boost::property_tree::ptree pt;
+  try {
+    boost::property_tree::ini_parser::read_ini(root_ + "path_config.ini", pt);
+    
+    // Iterate over all keys in the paths section of path_config.ini and add 
+    // them to the map
+    for (auto path : pt.get_child("paths"))
+      directories_.insert(std::pair<std::string, std::string>(path.first, path.second.data()));
+  }
+  catch (boost::property_tree::ini_parser_error err) {
+    std::stringstream error_message;
+    error_message << "Error reading path config, unable to access resources. File ("
+      << err.filename()
+      << " at line "
+      << err.line()
+      << "): "
+      << err.message();
+    // Printing the error directly to the console as the Path subsystem is 
+    // initialized before the logger. 
+    std::cout << error_message.str() << std::endl;
+    // TODO(Jelle): Offer to create a test file to indicate what directory the 
+    // engine considers its root directory. Shut down the Engine afterwards as 
+    // this is a critical error. 
+  }
+}
+
+const std::string & Path::operator[](const std::string directory) const {
+  if (directories_.count(directory) == 0) {
+    std::stringstream error_message;
+    std::string dir{ directory };
+    boost::to_upper(dir);
+    error_message << dir << "_DIRECTORY_NOT_FOUND";
+    return error_message.str();
+  }
+  return directories_.at(directory);
+}
+
+} // namespace
